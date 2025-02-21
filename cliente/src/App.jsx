bbafc5577fcf978266c8545c61b7a9e112af4a37
chris07054 <bricenobricenoca@uvm.edu.ve>
2025-02-21 18:02:02 -0400

    Archivo App y configurando tailwind.css

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,8 +1,52 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
+import Layout from "./components/Layout"
+import PrivateRoute from "./components/PrivateRoute"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Blog from "./pages/Blog"
+import Contact from "./pages/Contact"
+import Booking from "./pages/Booking"
+import Admin from "./pages/Admin"
+import Login from "./pages/Login"
+import Testimonials from "./pages/Testimonials"
+import Register from "./pages/Register"
+import { AuthProvider } from "./contexts/AuthProvider"
 
 function App() {
   return (
-    <p>funciona</p>
+    <AuthProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route
+              path="/booking"
+              element={
+                <PrivateRoute>
+                  <Booking />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/admin"
+              element={
+                <PrivateRoute adminOnly>
+                  <Admin />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Layout>
+        <Toaster position="top-right" />
+      </Router>
+    </AuthProvider>
   )
 }
 
